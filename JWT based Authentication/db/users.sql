1413028ff827f9d2d79593f3cb7accc0fbf502bd
Habib Rayhan <57207750+habibrayhan007@users.noreply.github.com>
2022-10-01 14:59:47 +0600

    Delete JWT based Authentication directory

--- JWT based Authentication/db/users.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `users` (
-  `id` varchar(255) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `registerd` datetime NOT NULL,
-  `last_login` datetime NOT NULL,
-  PRIMARY KEY(id)
-)
